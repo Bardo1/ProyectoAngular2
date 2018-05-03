85b24441a99389388096b3f33fa22d38935625cc
Bardo1 <walterrmz1@gmail.com>
2018-05-03 14:01:15 -0300

    if else

---
+++ AngularIntro/my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:any = 'appjlkljkljkjlk';
+   title1:string = 'Novedades';
+   show:boolean = true;
+}
